effe3aac5b59069392c1f586b7abded29ee527d4
nitayke <nitayke@gmail.com>
2021-04-28 11:39:26 +0300

    resize images .py

--- place.py
+++ place.py
@@ -5,7 +5,7 @@ from time import time
 from random import choice, randint
 import math
 
-BIG_SIZE = 2500
+BIG_SIZE = 250
 
 def rotate_image(image, angleInDegrees):
     h, w = image.shape[:2]
@@ -31,17 +31,18 @@ for filename in os.listdir(directory):
     s_img = cv2.imread(directory + filename)
     l_img = np.zeros((BIG_SIZE, BIG_SIZE, 3), np.uint8) # blank image
     l_img.fill(255)
-
-    s_img = rotate_image(s_img, randint(0, 360))
+    angle = randint(0, 90)
+    s_img = rotate_image(s_img, angle)
     height, width, _ = s_img.shape
     while BIG_SIZE < width or BIG_SIZE < height:
         s_img = cv2.resize(s_img, (int(0.5*width), int(0.5*height)), interpolation=cv2.INTER_AREA)
-        s_img = rotate_image(s_img, randint(0, 360))
+        angle = randint(0, 90)
+        s_img = rotate_image(s_img, angle)
         height, width, _ = s_img.shape
         print height, width
     x_offset = randint(0, (BIG_SIZE-width))
     y_offset = randint(0, BIG_SIZE-height)
     l_img[y_offset:y_offset+height, x_offset:x_offset+width] = s_img
 
-    cv2.imwrite('/home/lab1/nitay/buildings/big_images/' + filename, cv2.resize(l_img, (int(l_img.shape[1]/10), int(l_img.shape[0]/10)), interpolation=cv2.INTER_AREA))
-    print filename
+    cv2.imwrite('/home/lab1/nitay/buildings/big_images/' + filename, l_img)
+    print filename, angle
---
+++ resize_images.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+
+directory = "/home/lab1/nitay/buildings/dataset/"
+
+for filename in os.listdir(directory):
+    img = cv2.imread(directory + filename)
+
+    img = cv2.resize(img, (int(img.shape[1]/10), int(img.shape[0]/10)), interpolation=cv2.INTER_AREA)
+
+    cv2.imwrite(directory + filename, img)
