97c545916a553f65f4e336e89b72c8c3a6b09387
nitayke <nitayke@gmail.com>
2021-04-28 14:57:48 +0300

    .

--- find_rect.py
+++ find_rect.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 directory = "./big_images/"
 f = open('./coordinates', 'w+')
@@ -8,12 +9,25 @@ for filename in os.listdir(directory):
     gray = cv2.cvtColor(l_img, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    ((x, y), (width, height), angle) = cv2.minAreaRect(cnts[0])
+    print len(cnts)
+    im = np.zeros((250, 250, 3), np.uint8) # blank image
+    cv2.drawContours(im, cnts, -1, (0,255,0), 3)
+
+    ((x, y), (width, height), angle) = cv2.minAreaRect(cnts)
     (x, y, width, height, angle) = (round(x, 2), round(y, 2), round(width, 2), round(height, 2), round(angle, 2))
     d = {'x': x, 'y': y, 'width': width, 'height': height, 'angle': angle}
     f.write(filename + ': ' + str(d) + '\n')
     print filename
+    box = np.int0(cv2.boxPoints(((x, y), (width, height), angle) ))
+    cv2.drawContours(l_img, [box], 0, (36,255,12), 3)
+    # cv2.imshow('image ' + filename, l_img)
+    # cv2.waitKey()
+
+    numpy_horizontal = np.hstack((im, l_img))
+    numpy_horizontal_concat = np.concatenate((im, l_img), axis=1)
+    cv2.imshow(filename, numpy_horizontal_concat)
+    cv2.waitKey()
 
 f.close()
--- place.py
+++ place.py
@@ -22,15 +22,11 @@ def rotate_image(image, angleInDegrees):
     rot[0, 2] += ((b_w / 2) - img_c[0])
     rot[1, 2] += ((b_h / 2) - img_c[1])
 
-    outImg = cv2.warpAffine(image, rot, (b_w, b_h), flags=cv2.INTER_NEAREST, borderValue=(255,255,255))
+    outImg = cv2.warpAffine(image, rot, (b_w, b_h), flags=cv2.INTER_LINEAR, borderValue=(255,255,255))
     return outImg
 
 directory = "/home/lab1/nitay/buildings/small_dataset/"
-file = open('./sizes')
-corners = file.read()
-file.close()
 
-sizes = yaml.load(corners)
 file = open('./coordinates', 'w+')
 
 for filename in os.listdir(directory):
@@ -46,7 +42,6 @@ for filename in os.listdir(directory):
         s_img = rotate_image(s_img, angle)
         height, width, _ = s_img.shape
         print height, width
-        sizes[filename][0], sizes[filename][1] = sizes[filename][0] // 2, sizes[filename][1] // 2
     x_offset = randint(0, (BIG_SIZE-width))
     y_offset = randint(0, BIG_SIZE-height)
     l_img[y_offset:y_offset+height, x_offset:x_offset+width] = s_img
