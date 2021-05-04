c6abf94d236ea22e25e7da712c4be620c1ba7908
nitayke <nitayke@gmail.com>
2021-05-04 16:16:33 +0300

    angle changed to difference instead of ratio

--- scripts/communicate.py
+++ scripts/communicate.py
@@ -9,43 +9,28 @@ import numpy as np
 import json
 from math import pi
 
-def round1(num):
-    return round(num, 2)
+errors = []
 
 def callback(data):
     global j
 
+    if "j" not in globals():
+        return
+
     filename = data.header.frame_id
-    rect = [[j[filename]['x'], j[filename]['y']], [j[filename]['width'], j[filename]['height']], j[filename]['angle']]
+    rect = [j[filename]['x']/10, j[filename]['y']/10, j[filename]['width']/10, j[filename]['height']/10, j[filename]['angle']]
     
-    result = [[round1(10*data.rectangle.centerX), round1(10*data.rectangle.centerY)], [round1(10*data.rectangle.width),
-     round1(10*data.rectangle.height)], round1(data.rectangle.theta)]
+    result = [data.rectangle.centerX, data.rectangle.centerY, data.rectangle.width, data.rectangle.height, data.rectangle.theta]
 
-
-    if abs(abs(rect[2] - result[2]) - pi/2) < 0.1:
-        rect[1][0], rect[1][1] = rect[1][1], rect[1][0]
-        rect[2] = result[2]
-
-    # im2 = im1 = np.zeros((250, 250, 3), np.uint8)
-
-    # box = cv2.boxPoints(rect)
-    # box = np.int0(box)
-    # cv2.drawContours(im1,[box],0,(0,0,255),2)
-
-    # box1 = cv2.boxPoints(result)
-    # box1 = np.int0(box)
-    # cv2.drawContours(im2,[box1],0,(0,0,255),2)
-
-    # hori = np.concatenate((im1, im2), axis=1)
-
-    # cv2.imshow('name', hori)
-    # cv2.waitKey()
-
+    if abs(abs(rect[4] - result[4]) - pi/2) < 0.1:
+        rect[2], rect[3] = rect[3], rect[2]
     
-    print(rect)
-    print(result)
-
-    print()
+    try:
+        single_errors = [100 * abs(1 - (result[i] / rect[i])) for i in range(4)] + [100/360*abs(result[4] / rect[4])]
+        single_errors = [round(i, 3) for i in single_errors]
+        errors.append(single_errors)
+    except ZeroDivisionError:
+        pass
 
 rospy.init_node('test_img')
 
@@ -76,12 +61,6 @@ for filename in j.keys():
     
     pub.publish(ogrid)
 
-    # box = cv2.boxPoints(rect)
-    # box = np.int0(box)
-    # cv2.drawContours(im1,[box],0,(0,0,255),2)
-    # cv2.imshow('f', im1)
-    # cv2.waitKey()
+# rospy.spin()
 
-    # d = np.reshape(np.array(ogrid.data, dtype=np.uint8), (250, 250))
-
-rospy.spin()
+print(errors[:50])
