84bd54ac7c836f841a83b9800e1da9378d775e62
nitayke <nitayke@gmail.com>
2021-05-04 18:38:13 +0300

    files arranged

---
+++ scripts/3_place.py
@@ -0,0 +1,53 @@
+import os
+import cv2
+import numpy as np
+from random import randint
+from math import radians, cos, sin
+import yaml
+from constants import *
+
+def rotate_image(image, angleInDegrees):
+    h, w = image.shape[:2]
+    img_c = (w / 2, h / 2)
+
+    rot = cv2.getRotationMatrix2D(img_c, angleInDegrees, 1)
+
+    rad = radians(angleInDegrees)
+    sin1 = sin(rad)
+    cos1 = cos(rad)
+    b_w = int((h * abs(sin1)) + (w * abs(cos1)))
+    b_h = int((h * abs(cos1)) + (w * abs(sin1)))
+
+    rot[0, 2] += ((b_w / 2) - img_c[0])
+    rot[1, 2] += ((b_h / 2) - img_c[1])
+
+    outImg = cv2.warpAffine(image, rot, (b_w, b_h), flags=cv2.INTER_LINEAR, borderValue=(255,255,255))
+    return outImg
+
+directory = "../after_resize/"
+
+for filename_int in range(1, FILES_COUNT + 1):
+    filename = str(filename_int) + '.jpg'
+    s_img = cv2.imread(directory + filename)
+    l_img = np.zeros((IMAGE_SIZE, IMAGE_SIZE, 3), np.uint8) # blank image
+    l_img.fill(255)
+    angle = randint(0, 90)
+    s_img = rotate_image(s_img, angle)
+    height, width, _ = s_img.shape
+    while width > MAX_BUILDING_SIZE or height > MAX_BUILDING_SIZE:
+        height, width = int(height/2), int(width/2)
+        s_img = cv2.resize(s_img, (width, height), interpolation=cv2.INTER_AREA)
+        # angle = randint(0, 90)
+        # s_img = rotate_image(s_img, angle)
+    while width < MIN_BUILDING_SIZE or height < MIN_BUILDING_SIZE:
+        height, width = height*2, width*2
+        s_img = cv2.resize(s_img, (width, height), interpolation=cv2.INTER_AREA)
+        # angle = randint(0, 90)
+        # s_img = rotate_image(s_img, angle)
+
+    x_offset = randint(0, (IMAGE_SIZE-width))
+    y_offset = randint(0, IMAGE_SIZE-height)
+    l_img[y_offset:y_offset+height, x_offset:x_offset+width] = s_img
+    
+    cv2.imwrite('../final/' + filename, l_img)
+    print(filename)
---
+++ scripts/4_find_rect.py
@@ -0,0 +1,34 @@
+import json
+import cv2
+from math import radians
+import os
+import numpy as np
+from constants import *
+
+directory = "../final/"
+d = {}
+
+for filename_int in range(1, FILES_COUNT + 1):
+    filename = str(filename_int) + '.jpg'
+    l_img = cv2.imread(directory + filename)
+    gray = cv2.cvtColor(l_img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    im = np.zeros((SIZE, SIZE, 3), np.uint8) # blank image
+    if len(cnts) > 1:
+        tmp = cnts[0]
+        for i in cnts[1:]:
+            tmp = np.append(tmp, i, axis=0)
+        cnts = [tmp]
+    cv2.drawContours(im, cnts, 0, (0,255,0), 3)
+
+    ((x, y), (width, height), angle) = cv2.minAreaRect(cnts[0])
+    (x, y, width, height, angle) = (round(x, 2), round(y, 2), round(width, 2), round(height, 2), round(radians(angle), 2))
+    d[filename] = {'x': x, 'y': y, 'width': width, 'height': height, 'angle': angle}
+    print(filename)
+
+f = open('../rectangles.json', 'w')
+f.write(json.dumps(d, indent=3))
+f.close()
